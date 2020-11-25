6c4f04b25292f29b91ad5848ed6845704058cec3
Norman Hooper <nhooper@dimagi.com>
2020-11-25 15:24:10 +0200

    services use TelegramClient as context manager

--- mpact_api/mpact/services.py
+++ mpact_api/mpact/services.py
@@ -1,4 +1,7 @@
+from contextlib import asynccontextmanager
+
 from telethon import TelegramClient
+from telethon.hints import EntityLike
 
 from .constants import (
     BOT_TOKEN,
@@ -13,59 +16,70 @@ from .constants import (
 )
 from .logger import logger
 
-# Remember to use your own values from my.telegram.org!
-api_id = TELEGRAM_API_ID
-api_hash = TELEGRAM_API_HASH
-bot_token = BOT_TOKEN
-# loop = asyncio.get_event_loop()
-client = TelegramClient("anon", api_id, api_hash)
-bot = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
+
+async def login(data: dict):
+    async with get_anon_client() as client:
+        if CODE in data:
+            user_details = await client.sign_in(code=data[CODE])
+            return user_details
+        else:
+            # sending otp to phone number
+            code_request = await client.send_code_request(data[PHONE])
+            return code_request
 
 
-def connection(func):
-    async def inner_function(*args, **kwargs):
+async def logout():
+    async with get_anon_client() as client:
+        if await client.is_user_authorized():
+            await client.log_out()
+        return LOGOUT
+
+
+async def send_msg(entity: EntityLike, data: dict):
+    async with get_anon_client() as client:
+        if await client.is_user_authorized():
+            await client.send_message(entity, data[MESSAGE])
+    return MESSAGE_SENT
+
+
+async def get_dialogs():
+    async with get_anon_client() as client:
+        if await client.is_user_authorized():
+            dialogs = await client.get_dialogs()
+            return dialogs
+    return NOT_AUTHORIZED
+
+
+@asynccontextmanager
+async def get_anon_client_with_logger():
+    """
+    TelegramClient is already an async context manager. This context
+    manager just logs exceptions.
+    """
+    async with get_anon_client() as client:
         try:
-            # Start the client before we start serving
-            await client.connect()
-            results = await func(*args, **kwargs)
-            # After serving (near shutdown), clean up the client
-            await client.disconnect()
-            return results
+            yield client
         except Exception as exception:
             logger.exception(exception)
             raise
 
-    return inner_function
+
+def get_anon_client() -> TelegramClient:
+    """
+    Returns a TelegramClient with the session name "anon"
+
+    .. IMPORTANT::
+       Exposing a logged-in session over a web API allows **anyone** to
+       use that session, not just the person who logged in!
+
+    """
+    return get_telegram_client("anon")
 
 
-@connection
-async def login(data):
-    if CODE in data:
-        user_details = await client.sign_in(code=data[CODE])
-        return user_details
-    else:
-        # sending otp to phone number
-        code_request = await client.send_code_request(data[PHONE])
-        return code_request
+
+def start_bot_client() -> TelegramClient:
+    return get_telegram_client("bot").start(bot_token=BOT_TOKEN)
 
 
-@connection
-async def logout():
-    if await client.is_user_authorized():
-        await client.log_out()
-    return LOGOUT
-
-
-@connection
-async def send_msg(data):
-    if await client.is_user_authorized():
-        await client.send_message(-488152794, data[MESSAGE])
-    return MESSAGE_SENT
-
-
-@connection
-async def get_dialogs():
-    if await client.is_user_authorized():
-        dialogs = await client.get_dialogs()
-        return dialogs
-    return NOT_AUTHORIZED
+def get_telegram_client(session_name: str) -> TelegramClient:
+    return TelegramClient(session_name, TELEGRAM_API_ID, TELEGRAM_API_HASH)
