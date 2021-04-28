a7e73a5f8007b29129aeb32943f2bce48a7a57ab
Cory Zue <czue@dimagi.com>
2021-04-28 13:54:33 +0200

    extract constant

--- mpact/tests/test_schedules.py
+++ mpact/tests/test_schedules.py
@@ -2,14 +2,13 @@ from django.test import TestCase
 from django_celery_beat.models import PeriodicTask, ClockedSchedule
 
 from mpact.models import GroupChat, ScheduledMessage
-from mpact.tests.util import run_test_schedule
+from mpact.tests.util import run_test_schedule, TEST_GROUP_ID
 
 
 class ScheduleTestCase(TestCase):
-    GROUP_ID = '12345'
 
     def test_happy_upload(self):
-        GroupChat.objects.create(id=self.GROUP_ID)
+        GroupChat.objects.create(id=TEST_GROUP_ID)
         result = run_test_schedule()['data']
         self.assertEqual([], result['bad titles'])
         self.assertEqual(6, ScheduledMessage.objects.count())
--- mpact/tests/util.py
+++ mpact/tests/util.py
@@ -2,6 +2,7 @@ import os
 
 from mpact.services import schedule_messages
 
+TEST_GROUP_ID = '12345'
 
 def run_test_schedule():
     schedule_file = os.path.join(os.path.dirname(__file__), 'data', 'test_schedules.xlsx')
