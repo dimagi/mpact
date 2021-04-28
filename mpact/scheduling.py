ed8210bdc40144bbaa96305a0d7b8b01b7465ea7
Cory Zue <czue@dimagi.com>
2021-04-28 14:49:01 +0200

    clear existing schedule when creating a new schedule

--- mpact/scheduling.py
+++ mpact/scheduling.py
@@ -6,9 +6,23 @@ from django_celery_beat.models import ClockedSchedule, PeriodicTask
 
 
 def rebuild_schedule_for_group(group, messages=None):
-    start_date_time = parse(f"{group.schedule_start_date} {group.schedule_start_time}")
     messages = messages or group.scheduled_messages.all()
+    disable_tasks_for_group(group)
+    create_new_tasks_for_group(group, messages)
 
+
+def disable_tasks_for_group(group):
+    """
+    Disables any scheduled message tasks associated with the group.
+    """
+    tasks = get_periodic_tasks_for_group(group)
+    tasks.update(
+        enabled=False,
+    )
+
+
+def create_new_tasks_for_group(group, messages):
+    start_date_time = parse(f"{group.schedule_start_date} {group.schedule_start_time}")
     for message in messages:
         schedule, __ = ClockedSchedule.objects.get_or_create(
             clocked_time=start_date_time + timedelta(days=message.day),
--- mpact/tests/test_periodic_tasks.py
+++ mpact/tests/test_periodic_tasks.py
@@ -36,6 +36,7 @@ class PeriodicTaskTestCase(TestCase):
             args = json.loads(task.args)
             self.assertEqual(self.GROUP_1_ID, args[0])
             self.assertEqual(f'g1 m{i + 1}', args[1])
+            self.assertTrue(task.enabled)
 
         g2_tasks = get_periodic_tasks_for_group(g2)
         self.assertEqual(4, len(g2_tasks))
@@ -43,3 +44,38 @@ class PeriodicTaskTestCase(TestCase):
             args = json.loads(task.args)
             self.assertEqual(self.GROUP_2_ID, args[0])
             self.assertEqual(f'g2 m{i + 1}', args[1])
+            self.assertTrue(task.enabled)
+
+    def test_update_schedule(self):
+        g1 = GroupChat.objects.create(id=self.GROUP_1_ID)
+        # have to refresh from DB to get dates/times back properly
+        g1.refresh_from_db()
+        for i in range(1, 5):
+            ScheduledMessage.objects.create(
+                group=g1,
+                day=i,
+                message=f'g1 m{i}'
+            )
+        # this creates the first batch of periodic tasks
+        rebuild_schedule_for_group(g1)
+
+        # clear and create a new schedule
+        ScheduledMessage.objects.filter(group=g1).delete()
+        for i in range(1, 5):
+            ScheduledMessage.objects.create(
+                group=g1,
+                day=i,
+                message=f'g1 m{i} v2'
+            )
+
+        rebuild_schedule_for_group(g1)
+        g1_tasks = get_periodic_tasks_for_group(g1)
+        self.assertEqual(8, len(g1_tasks))
+        enabled_tasks = g1_tasks.filter(enabled=True)
+        disabled_tasks = g1_tasks.filter(enabled=False)
+        self.assertEqual(4, enabled_tasks.count())
+        self.assertEqual(4, disabled_tasks.count())
+        for i, task in enumerate(enabled_tasks):
+            args = json.loads(task.args)
+            self.assertEqual(self.GROUP_1_ID, args[0])
+            self.assertEqual(f'g1 m{i + 1} v2', args[1])
