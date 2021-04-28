4eaff058c1dbfd011734a82279d290e4a73a513e
Cory Zue <czue@dimagi.com>
2021-04-28 13:52:56 +0200

    extract function

--- mpact/tests/test_schedules.py
+++ mpact/tests/test_schedules.py
@@ -1,9 +1,8 @@
-import os
 from django.test import TestCase
 from django_celery_beat.models import PeriodicTask, ClockedSchedule
 
 from mpact.models import GroupChat, ScheduledMessage
-from mpact.services import schedule_messages
+from mpact.tests.util import run_test_schedule
 
 
 class ScheduleTestCase(TestCase):
@@ -11,10 +10,7 @@ class ScheduleTestCase(TestCase):
 
     def test_happy_upload(self):
         GroupChat.objects.create(id=self.GROUP_ID)
-        schedule_file = os.path.join(os.path.dirname(__file__), 'data', 'test_schedules.xlsx')
-        with open(schedule_file, 'rb') as f:
-            result = schedule_messages(f)['data']
-
+        result = run_test_schedule()['data']
         self.assertEqual([], result['bad titles'])
         self.assertEqual(6, ScheduledMessage.objects.count())
         self.assertEqual(6, ClockedSchedule.objects.count())
---
+++ mpact/tests/util.py
@@ -0,0 +1,9 @@
+import os
+
+from mpact.services import schedule_messages
+
+
+def run_test_schedule():
+    schedule_file = os.path.join(os.path.dirname(__file__), 'data', 'test_schedules.xlsx')
+    with open(schedule_file, 'rb') as f:
+        return schedule_messages(f)
