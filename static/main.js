284d8dd2fbd1205d18ff57e93b5f353cb7e07fbf
Brian DeRenzi <bderenzi@dimagi.com>
2021-05-05 11:06:52 +0200

    Removed unused jQuery, lodash and vue-tel-input

--- static/main.js
+++ static/main.js
@@ -1,6 +1,4 @@
 import Vue from 'vue';
-import VueTelInput from 'vue-tel-input';
-import jQuery from 'jquery';
 import App from './App.vue';
 import Toast from './src/components/Toast.vue';
 import Api from './src/services/Api'
@@ -8,16 +6,11 @@ import router from './src/router/index';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './store'
-import _ from 'lodash'
 import MessageService from './src/services/MessageService';
 import dateHelpers from './src/utils/helpers/dateHelpers';
 
-Vue.use(VueTelInput);
-
 Vue.prototype.$http = Api;
-Vue.prototype.$ = jQuery;
 
-window.$ = jQuery;
 
 Vue.component('Toast', Toast);
 
