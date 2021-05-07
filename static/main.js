19144d3e7afc45f80bbd5c787cdb2195cb2b36c2
Brian DeRenzi <bderenzi@dimagi.com>
2021-05-07 11:44:28 +0200

    More code cleanup
    Remove more references to jquery, lowdash and the vue tel input.

--- static/main.js
+++ static/main.js
@@ -1,6 +1,4 @@
 import Vue from 'vue';
-import VueTelInput from 'vue-tel-input';
-import jQuery from 'jquery';
 import App from './App.vue';
 import Toast from './src/components/Toast.vue';
 import Api from './src/services/Api'
@@ -8,16 +6,9 @@ import router from './src/router/index';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './store'
-import _ from 'lodash'
-import MessageService from './src/services/MessageService';
 import dateHelpers from './src/utils/helpers/dateHelpers';
 
-Vue.use(VueTelInput);
-
 Vue.prototype.$http = Api;
-Vue.prototype.$ = jQuery;
-
-window.$ = jQuery;
 
 Vue.component('Toast', Toast);
 
@@ -39,32 +30,28 @@ new Vue({
     socket.onmessage = async event => {
       const currentMessages = this.$store.state.messages
       const newMessage = JSON.parse(event.data).message
-      const id = newMessage.id
-      newMessage._id = newMessage.id,
-      newMessage.content = newMessage.message
-      newMessage.timestamp = dateHelpers.convertTime(newMessage.date)
-      newMessage.date = dateHelpers.convertDate(newMessage.date)
-      newMessage.username = newMessage.sender_name
-      newMessage.roomId = newMessage.room_id
-      currentMessages.push(newMessage)
       const activeChannel = this.$store.state.active_channel
+      // IF the new message isn't from the current room, just update the unread count and ignore the message
       if (activeChannel.toString() !== newMessage.room_id.toString()) {
         const currentUnreadMessages = this.$store.state.unread_messages
         currentUnreadMessages[newMessage.room_id] = currentUnreadMessages[newMessage.room_id] + 1
         this.$store.dispatch('update_unread_messages', currentUnreadMessages)
       }
+      // Otherwise, let's add it to our messages array in the store so it displays on screen
       else {
-        const params = {
-          roomId: newMessage.room_id,
-          offset: 0,
-          limit: 50
-        }
-        const data = await MessageService.getIndividualMessages(params);
+        newMessage._id = newMessage.id,
+        newMessage.content = newMessage.message
+        newMessage.timestamp = dateHelpers.convertTime(newMessage.date)
+        newMessage.date = dateHelpers.convertDate(newMessage.date)
+        newMessage.username = newMessage.sender_name
+        newMessage.roomId = newMessage.room_id
+        currentMessages.push(newMessage)
+        this.$store.dispatch('update_messages', {roomId: activeChannel, msgs: currentMessages})
       }
-      this.$store.dispatch('update_messages', {roomId: activeChannel, msgs: currentMessages})
     }
 
     socket.onclose = event => {
+      // TODO: Alert the user and tell them to refresh the page
       console.log('websocket connection closed')
       this.connected = false
     }
