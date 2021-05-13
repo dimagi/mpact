345b9fc65087921f1a7f401850d483084b458580
Brian DeRenzi <bderenzi@dimagi.com>
2021-05-13 16:00:23 +0200

    Honestly, I don't even know what that was for.

--- static/src/views/Chat.vue
+++ static/src/views/Chat.vue
@@ -20,13 +20,6 @@
       @message-action-handler='messageActionHandler($event)'
       :text-messages='textMessages'
       :load-first-room='false' :show-files='false' :show-audio='false' :show-reaction-emojis='false' :show-add-room='false'>
-      <template #dropdown-icon>
-        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' viewBox='0 0 16 16'>
-          <path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8
-          10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6
-            6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z' />
-        </svg>
-      </template>
       <template v-slot:rooms-header>
         <rooms-list-header />
       </template>
