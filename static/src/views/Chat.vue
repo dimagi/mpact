d2c60ae697bfedf404c9981ffd97a0ec3be6e340
Brian DeRenzi <bderenzi@dimagi.com>
2021-05-13 16:23:43 +0200

    clean clean

--- static/src/views/Chat.vue
+++ static/src/views/Chat.vue
@@ -21,9 +21,7 @@
       :load-first-room='false' :show-files='false' :show-audio='false' :show-reaction-emojis='false' :show-add-room='false'>
       <template #dropdown-icon>
         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' viewBox='0 0 16 16'>
-          <path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8
-          10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6
-            6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z' />
+          <path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z' />
         </svg>
       </template>
       <template v-slot:rooms-header>
@@ -69,15 +67,13 @@ export default {
         ROOMS_EMPTY: 'Add your configured bot to a Telegram group and refresh to get started',
         ROOM_EMPTY: 'No group or individual chat selected',
       },
-      messageActions: [
-        {
+      messageActions: [{
           name: 'flagMessage',
           title: 'Flag Message',
-        },
-        {
+        }, {
           name: 'replyMessage',
           title: 'Reply',
-        },
+        }
       ],
       styles: {
         icons: {
