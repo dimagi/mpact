560328971dcdc474ed306896919e24eff6f250b5
Cory Zue <czue@dimagi.com>
2021-05-20 15:20:26 +0200

    add dummy secret key

--- telegram_bot/settings_heroku.py
+++ telegram_bot/settings_heroku.py
@@ -1,5 +1,10 @@
+import os
 import django_heroku
+from django.core.management.utils import get_random_secret_key
+
 django_heroku.settings(locals())
+
+SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
 # fix ssl mixed content issues
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
