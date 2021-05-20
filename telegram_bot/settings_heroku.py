1dd1a5386b979e0ccafc99be56aad8df93b8ee09
Cory Zue <czue@dimagi.com>
2021-05-20 15:43:10 +0200

    set static_root

--- telegram_bot/settings_heroku.py
+++ telegram_bot/settings_heroku.py
@@ -1,6 +1,9 @@
+from .settings import *
 import django_heroku
 
 django_heroku.settings(locals())
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_out')
 
 # fix ssl mixed content issues
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
