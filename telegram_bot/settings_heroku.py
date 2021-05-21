d7527cfe8c2507f3f034ca5663a9dd1878952031
Cory Zue <czue@dimagi.com>
2021-05-21 09:05:34 +0200

    try fixing redis

--- telegram_bot/settings_heroku.py
+++ telegram_bot/settings_heroku.py
@@ -3,6 +3,20 @@ import django_heroku
 
 django_heroku.settings(locals())
 
+# redis setup
+REDIS_URL = os.environ.get('REDIS_URL')
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [REDIS_URL],
+        },
+    }
+}
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_out')
 
 # fix ssl mixed content issues
